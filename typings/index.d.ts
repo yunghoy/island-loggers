89e8c96cbb15a64efbc4ca11b5887aca1bca01a4
heycalmdown <heyacct@gmail.com>
2016-11-16 18:17:28 +0900

    Apply ts-2.0 and `@types`

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
-/// <reference path="modules/winston/index.d.ts" />
